51a9421629f4e13a83de92d205870406bf653349
Julian Silerio <julian.silerio@gmail.com>
2017-10-24 00:53:00 -0400

    trip add new_trip form, no html tho

--- project/server/trip/forms.py
+++ project/server/trip/forms.py
@@ -2,32 +2,12 @@
 
 
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField
-from wtforms.validators import DataRequired, Email, Length, EqualTo
+from wtforms import StringField, DateField
+from wtforms.validators import DataRequired
 
+class NewTripForm(FlaskForm):
+    name = StringField('Name', [DataRequired()])
+    location = StringField('Location', [DataRequired()])
+    start_date = DateField('Start date', [DataRequired()])
+    end_date =  DateField('End date', [DataRequired()])
 
-class LoginForm(FlaskForm):
-    email = StringField('Email Address', [DataRequired(), Email()])
-    password = PasswordField('Password', [DataRequired()])
-
-
-class RegisterForm(FlaskForm):
-    email = StringField(
-        'Email Address',
-        validators=[
-            DataRequired(),
-            Email(message=None),
-            Length(min=6, max=40)
-        ]
-    )
-    password = PasswordField(
-        'Password',
-        validators=[DataRequired(), Length(min=6, max=25)]
-    )
-    confirm = PasswordField(
-        'Confirm password',
-        validators=[
-            DataRequired(),
-            EqualTo('password', message='Passwords must match.')
-        ]
-    )
--- project/server/trip/views.py
+++ project/server/trip/views.py
@@ -1,4 +1,4 @@
-# project/server/user/views.py
+# project/server/trip/views.py
 
 
 #################
@@ -10,64 +10,37 @@ from flask import render_template, Blueprint, url_for, \
 from flask_login import login_user, logout_user, login_required
 
 from project.server import bcrypt, db
-from project.server.models import User
-from project.server.user.forms import LoginForm, RegisterForm
+from project.server.models import Trip
+from project.server.trip.forms import NewTripForm
 
 ################
 #### config ####
 ################
 
-user_blueprint = Blueprint('user', __name__,)
+trip_blueprint = Blueprint('trip', __name__,)
 
 
 ################
 #### routes ####
 ################
 
-@user_blueprint.route('/register', methods=['GET', 'POST'])
-def register():
-    form = RegisterForm(request.form)
+@trip_blueprint.route('/new_trip', methods=['GET', 'POST'])
+def new_trip():
+    form = NewTripForm(request.form)
     if form.validate_on_submit():
-        user = User(
-            email=form.email.data,
-            password=form.password.data
+        trip = Trip(
+            name=form.name.data,
+            location=form.location.data,
+            start_date=form.start_date.data,
+            end_date=form.end_date.data
         )
-        db.session.add(user)
+        db.session.add(location)
         db.session.commit()
 
-        login_user(user)
 
-        flash('Thank you for registering.', 'success')
-        return redirect(url_for("user.members"))
+        flash('New trip {} created.'.format(form.name.data), 'success')
+        return redirect(url_for("main.home"))
 
-    return render_template('user/register.html', form=form)
-
-
-@user_blueprint.route('/login', methods=['GET', 'POST'])
-def login():
-    form = LoginForm(request.form)
-    if form.validate_on_submit():
-        user = User.query.filter_by(email=form.email.data).first()
-        if user and bcrypt.check_password_hash(
-                user.password, request.form['password']):
-            login_user(user)
-            flash('You are logged in. Welcome!', 'success')
-            return redirect(url_for('user.members'))
-        else:
-            flash('Invalid email and/or password.', 'danger')
-            return render_template('user/login.html', form=form)
-    return render_template('user/login.html', title='Please Login', form=form)
+    return render_template('trip/new_trip.html', form=form)
 
 
-@user_blueprint.route('/logout')
-@login_required
-def logout():
-    logout_user()
-    flash('You were logged out. Bye!', 'success')
-    return redirect(url_for('main.home'))
-
-
-@user_blueprint.route('/members')
-@login_required
-def members():
-    return render_template('user/members.html')
