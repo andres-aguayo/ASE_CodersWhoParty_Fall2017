9a6da7ceec3aaa80b4a4ceabb212250fab7bdbda
Julian Silerio <julian.silerio@gmail.com>
2017-11-21 14:28:12 +0100

    update create_db.sh

--- create_db.sh
+++ create_db.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
+# make sure to run this in python3
 
+python manage.py drop_db
 python manage.py create_db
 python manage.py db init
 python manage.py db migrate
-python manage.py create_admin
+#python manage.py create_admin
 python manage.py create_data
